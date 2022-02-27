40276c929c514bd1de4464b35ac6364b418dfcbc
Nict <71667497+NictX@users.noreply.github.com>
2022-02-27 19:58:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -48,17 +48,9 @@ uint64_t nextPrime(uint64_t value) {
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t n = 2;
   uint64_t summ = 0;
-  while (n < hbound) {
-    summ += n;
-    int x = 1;
-    while (x != 0) {
-      n++;
-      x = 0;
-      for (int y = 2; y < n; y++) {
-        if (n % y == 0) {
-          x++;
-        }
-      }
+  for (uint64_t  i = 2; i < hbound; i++) {
+    if (checkPrime (i)) {
+	    summ += i;
     }
   }
   return summ;
