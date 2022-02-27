c68bac2728cd2a34b92ecf572f3a9ba22b42e8e0
Nict <71667497+NictX@users.noreply.github.com>
2022-02-27 16:43:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,60 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  for (int i = 2; i < value; i++) {
+    if (value % i == 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  int n = 2;
+  int value = n;
+  for (int i = 1; i < value; i++) {
+    int x = 1;
+    while (x != 0) {
+      n++;
+      x = 0;
+      for (int y = 2; y < n; y++) {
+        if (n % y == 0) {
+          x++;
+        }
+      }
+    }
+  }
+  return n;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  int n = value;
+  int x = 1;
+  while (x != 0) {
+    n++;
+    x = 0;
+    for (int y = 2; y < n; y++) {
+      if (n % y == 0) {
+        x++;
+      }
+    }
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  int n = 2;
+  int summ = 0;
+  while (n < 8) {
+    summ += n;
+    int x = 1;
+    while (x != 0) {
+      n++;
+      x = 0;
+      for (int y = 2; y < n; y++) {
+        if (n % y == 0) {
+          x++;
+        }
+      }
+    }
+  }
 }
